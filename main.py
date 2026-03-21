6b9253bf1806f602a0bf97047a4669da3e21a38a
HowZen <howzen.hc@gmail.com>
2026-03-22 03:02:56 +0800

    fix: 抑制 PaddleX/Paddle 框架噪音 log（C++ INFO、ccache 警告等）

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ import warnings
 
 # 抑制第三方套件噪音
 os.environ["PADDLE_PDX_DISABLE_MODEL_SOURCE_CHECK"] = "True"
+os.environ["PADDLEX_LOG_LEVEL"] = "ERROR"
+os.environ["FLAGS_minloglevel"] = "2"  # 抑制 Paddle C++ INFO/WARNING
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 warnings.filterwarnings("ignore", message="urllib3.*doesn't match a supported version")
+warnings.filterwarnings("ignore", message="No ccache found")
 
 from PyQt6.QtWidgets import QApplication
 
@@ -18,8 +21,8 @@ logging.basicConfig(
 )
 
 # 將 PaddlePaddle/PaddleX 的 log 層級提高到 WARNING，只保留自己的 INFO log
-for _name in ("paddle", "paddlex", "paddleocr"):
-    logging.getLogger(_name).setLevel(logging.WARNING)
+for _name in ("paddle", "paddlex", "paddleocr", "ppocr"):
+    logging.getLogger(_name).setLevel(logging.ERROR)
 
 
 def main():
